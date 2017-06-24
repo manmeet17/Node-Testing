2b3fd48f933101f815c8fc73b3dcc63ebced2ce0
Manmeet Tarun <meetutarun16@gmail.com>
2017-06-24 17:36:20 +0530

    Added Spies

---
+++ spies/app.js
@@ -0,0 +1,11 @@
+const db = require('./db');
+
+module.exports.handleSignup = (email,password)=>{
+  //Check if email exists
+  //Save user to database
+  db.saveUser({
+    email,
+    password
+  });
+  //Send welcome email
+};
